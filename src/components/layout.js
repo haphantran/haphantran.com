270771fd22cf67f10012af056c7ab379a506c5c0
HaPhan Tran <47532906+haphantran@users.noreply.github.com>
2020-02-07 22:38:36 -0500

    adding material ui container to layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,15 +3,35 @@ import React from "react"
 import Header from "./header"
 import Footer from "./footer"
 import "../styles/index.scss"
-import layoutStyles from "./layout.module.scss"
+import { Container, CssBaseline } from "@material-ui/core"
 
 const Layout = props => {
   return (
-    <div className={layoutStyles.container}>
-      <Header />
-      <div className={layoutStyles.content}>{props.children}</div>
-      <Footer />
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container
+        maxWidth="md"
+        maxWidth="md"
+        style={{
+          minHeight: "100vh",
+          padding: "1rem",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Header />
+        <Container
+          style={{
+            padding: "1rem",
+            margin: 0,
+            flexGrow: 1,
+          }}
+        >
+          <div>{props.children}</div>
+        </Container>
+        <Footer />
+      </Container>
+    </React.Fragment>
   )
 }
 
