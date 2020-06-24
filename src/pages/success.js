666d7435656f4207958b86b40ffe303b9d634e45
haphan tran <haphantran@gmail.com>
2020-06-24 17:12:08 -0400

    fixing form again2

--- src/pages/success.js
+++ src/pages/success.js
@@ -7,8 +7,8 @@ const Success = () => {
   return (
     <Layout>
     <SEO title="Thank you"/>
-      <main className="error-page">
-    <div className="error-container">
+      <main className="success-page">
+    <div className="success-container">
       <h1>Thank you for your message. I will reply soon.</h1>
       <Link to="/" className='btn'>back home</Link>
     </div>
